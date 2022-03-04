f200c2dea62025641f9f950527301b1dda929599
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-03-04 16:37:05 -0500

    Remove starter code and replace with call to a weather api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
+  // Use a helpers to transform data into just whats needed
+  // Pass clean data to another component and create 
   useEffect(()=> {
     axios.get('http://api.openweathermap.org/data/2.5/weather?lat=75.6972&lon=45.4215&appid=c876465f1aa8174a67435e8f65ead6ce')
       .then(res => console.log("weather forecast data", res));
