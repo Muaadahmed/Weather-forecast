7808248379989777d4b31a0cd43f186cb95d796b
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2022-03-04 16:45:08 -0500

    Moved weather api token to env file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect} from 'react';
 import axios from 'axios';
 
 function App() {
-  // Use a helpers to transform data into just whats needed
-  // Pass clean data to another component and create 
+  // Use helpers to transform data into just whats needed
+  // Pass clean data to another component use that component to add the icon, day, and temp
   useEffect(()=> {
-    axios.get('http://api.openweathermap.org/data/2.5/weather?lat=75.6972&lon=45.4215&appid=c876465f1aa8174a67435e8f65ead6ce')
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=75.6972&lon=45.4215&appid={APPID}`)
       .then(res => console.log("weather forecast data", res));
   },[]);
   return (
